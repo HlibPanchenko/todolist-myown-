a3a0a723789283ac3f42cfdc9737f80556be7a88
HlibPanchenko <glebpanchenk7@gmail.com>
2023-01-29 12:12:32 +0200

    fixed empty input

--- todo.js
+++ todo.js
@@ -4,10 +4,12 @@ const taskList = document.querySelector(".todo__list"); //ul
 const emptyLi = document.querySelector(".todo__empty");
 const modal = document.querySelector("._modal"); // модальное окно
 
+//Events
 form.addEventListener("submit", addTask);
 taskList.addEventListener("click", deleteTask);
 taskList.addEventListener("click", openModal);
-// checkEmptyList();
+modal.addEventListener("click", closeModal);
+modal.addEventListener("click", editTextInput);
 
 let arrayOfTasksForLS = [];
 
@@ -47,42 +49,7 @@ if (taskList.children.length > 0) {
   emptyLi.classList.add("none");
 }
 
-// console.log(localStorage.getItem("task"));
-// if (localStorage.getItem("task")) {
-//   let arrOfTasksFromLS = localStorage.getItem("task");
-//   let parsedArrTasksFromLS = JSON.parse(arrOfTasksFromLS);
-//   //   console.log(arrOfTasksFromLS);
-//   //   console.log(parsedArrTasksFromLS);
-
-//   parsedArrTasksFromLS.forEach((task) => {
-//     //  console.log(task);
-//     arrayOfTasksForLS.push(task);
-//     const markup = `<li class="todo__task" id="${task.id}">
-// 	 <div class="todo__left">
-// 		<p>
-// 		  ${task.text}
-// 		</p>
-// 	 </div>
-// 	 <div class="todo__right button-todo">
-// 		<button class="button-todo__green" id="done-button">
-// 		  <img class="button-todo" src="/img/tick.svg" alt="" />
-// 		</button>
-// 		<button class="button-todo__red" id="delete-button">
-// 		  <img class="button-todo" src="/img/cross.svg" alt="" />
-// 		</button>
-//     <button class="button-todo__edit" id="edit-button">
-//                   <img class="button-todo" src="/img/pencil.png" alt="" />
-//                 </button>
-// 	 </div>
-//   </li>`;
-
-//     taskList.insertAdjacentHTML("beforeend", markup);
-//   });
-
-//   if (taskList.children.length > 0) {
-//     emptyLi.classList.add("none");
-//   }
-// }
+// Функции
 
 function addTask(e) {
   // console.log(taskList.children);
@@ -150,19 +117,6 @@ function deleteTask(e) {
       emptyLi.classList.remove("none");
     }
 
-    //   console.log(e.target.closest("li").id);
-
-    // удаляет объекты с LS
-    // let arrOfTasksFromLS = localStorage.getItem("task");
-    // let parsedArrTasksFromLS = JSON.parse(arrOfTasksFromLS);
-    // console.log(parsedArrTasksFromLS);
-    // let indexOfDeletedEl = parsedArrTasksFromLS.findIndex(
-    //   (el) => el.id == e.target.closest("li").id
-    // );
-    // console.log(indexOfDeletedEl);
-    // // parsedArrTasksFromLS.splice(indexOfDeletedEl, 1);
-    // arrayOfTasksForLS.splice(indexOfDeletedEl, 1);
-
     const id = task.id;
 
     // Находим индекс задачи в массиве
@@ -177,13 +131,6 @@ function deleteTask(e) {
     arrayOfTasksForLS.splice(index, 1);
     // localStorage.setItem("task", JSON.stringify(parsedArrTasksFromLS));
     localStorage.setItem("task", JSON.stringify(arrayOfTasksForLS));
-
-    //   parsedArrTasksFromLS.forEach((taskInLs) => {
-    // 	if (e.target.closest("li").id == taskInLs.id) {
-    // 		// Ищем индекс этого элемента в массиве, чтобы потом его по этому индексу удалить
-    // 		parsedArrTasksFromLS.findIndex(el => el.id == e.target.closest("li").id);
-    // 	}
-    //   })
 
     task.remove();
   }
@@ -204,7 +151,7 @@ function openModal(e) {
     let textFromInput = liWeWantToEdit.querySelector("p").textContent.trim();
     console.log(textFromInput);
     let markupModal = `
-    <input class="modal-callback__input" type="text" value="${textFromInput}" autofocus id="${liWeWantToEdit.id}">
+    <input class="modal-callback__input" type="text" value="${textFromInput}" autofocus required id="${liWeWantToEdit.id}">
     <button class="modal-callback__btn" type="submit"> Внести изменения </button>
     `;
     document
@@ -215,8 +162,6 @@ function openModal(e) {
     console.log(inputEdit);
   }
 }
-
-modal.addEventListener("click", closeModal);
 
 function closeModal(e) {
   if (
@@ -233,50 +178,36 @@ function closeModal(e) {
   }
 }
 
-// вынес в глобальную область видимости чтобы другие функции видели
-// let newText = '';
-
-// document.querySelector('.modal-callback__btn').addEventListener('click', editTextInput);
 // внести изменения которые ввели в инпут в модальном окне
-modal.addEventListener("click", editTextInput);
-
 function editTextInput(e) {
   if (e.target.classList.contains("modal-callback__btn")) {
     // arrayOfTasksForLS = [];
 
     let inputInModal = document.querySelector(".modal-callback__input");
-    // console.log(inputInModal);
     let newText = inputInModal.value;
-    console.log(newText);
+    // console.log(newText);
 
-    console.log(arrayOfTasksForLS);
-    // по нажатию на кнопку меняется массив с данными, меняем value у таски и закрываем модальное окно
-    arrayOfTasksForLS.forEach((el) => {
-      if (el.id == inputInModal.id) {
-        el.text = newText;
-        console.log(el);
-      }
+    // console.log(arrayOfTasksForLS);
+    // вносим изменения если пользователь ввел что-то в инпут
+    if (newText) {
+      // по нажатию на кнопку меняется массив с данными, меняем value у таски и закрываем модальное окно
+      arrayOfTasksForLS.forEach((el) => {
+        if (el.id == inputInModal.id) {
+          el.text = newText;
+          console.log(el);
+        }
 
-      console.log(arrayOfTasksForLS);
-    });
-    // вносим изменения в LS
-    localStorage.setItem("task", JSON.stringify(arrayOfTasksForLS));
+        // console.log(arrayOfTasksForLS);
+      });
+      // вносим изменения в LS
+      localStorage.setItem("task", JSON.stringify(arrayOfTasksForLS));
 
-    // отрисовываем
-    // let arrOfTasksFromLS = localStorage.getItem("task");
-    // let parsedArrTasksFromLS = JSON.parse(arrOfTasksFromLS);
-    // console.log(arrOfTasksFromLS);
-    // console.log(parsedArrTasksFromLS);
+      // отрисовываем
 
-    // arrayOfTasksForLS = [];
-    // вот тут ниже надо исправлять (строки 277-301)
-    taskList.innerHTML = ""; // очищаем разметку потому что дублировалось когда вносили изменения в таску
-    // и уже в расчищеную разметку вставляем обновленные liшки
-    arrayOfTasksForLS.forEach((task) => {
-      //  console.log(task);
-
-      // arrayOfTasksForLS.push(task);
-      const markup = `<li class="todo__task" id="${task.id}">
+      taskList.innerHTML = ""; // очищаем разметку потому что дублировалось когда вносили изменения в таску
+      // и уже в расчищеную разметку вставляем обновленные liшки
+      arrayOfTasksForLS.forEach((task) => {
+        const markup = `<li class="todo__task" id="${task.id}">
      <div class="todo__left">
     	<p>
     	  ${task.text}
@@ -295,8 +226,9 @@ function editTextInput(e) {
      </div>
     </li>`;
 
-      taskList.insertAdjacentHTML("beforeend", markup);
-    });
+        taskList.insertAdjacentHTML("beforeend", markup);
+      });
+    }
 
     if (taskList.children.length > 0) {
       emptyLi.classList.add("none");
